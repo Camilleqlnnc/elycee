3a66cda3e9c885a362290c99b20f5a608450a013
francasix <francasix@outlook.com>
2015-08-05 23:34:19 +0200

    fix stats on status published

--- site/src/StatsBundle/Controller/DefaultController.php
+++ site/src/StatsBundle/Controller/DefaultController.php
@@ -37,17 +37,17 @@ class DefaultController extends Controller
     	$em = $doctrine->getManager();
     	
     	// total articles
-        $query = $em->createQuery('SELECT COUNT(a.id) FROM SpljBundle:Article a WHERE a.status != 2');
+        $query = $em->createQuery('SELECT COUNT(a.id) FROM SpljBundle:Article a WHERE a.status = 1');
         $articleCount = $query->getSingleResult();
         $stats->setTotalArticle($articleCount[1]);
 
 		// total qcm
-        $query = $em->createQuery('SELECT COUNT(m.id) FROM SpljBundle:Mcq m WHERE m.status != 2');
+        $query = $em->createQuery('SELECT COUNT(m.id) FROM SpljBundle:Mcq m WHERE m.status = 1');
         $mcqCount = $query->getSingleResult();
         $stats->setTotalMcq($mcqCount[1]);
 
         // total questions
-        $query = $em->createQuery('SELECT COUNT(q.id) FROM SpljBundle:Question q, SpljBundle:Mcq m WHERE m.id = q.idQcm AND m.status != 2');
+        $query = $em->createQuery('SELECT COUNT(q.id) FROM SpljBundle:Question q, SpljBundle:Mcq m WHERE m.id = q.idQcm AND m.status = 1');
         $questionCount = $query->getSingleResult();
         $stats->setTotalQuestion($questionCount[1]);
 
@@ -67,19 +67,19 @@ class DefaultController extends Controller
                 (SELECT count(m.id)
                     FROM SpljBundle:Mcq m
                     WHERE m.userId = u.id
-                    AND m.status != 2
+                    AND m.status = 1
                     AND u.roles LIKE :teacher
                     GROUP BY u.username) AS mcqs, 
                 (SELECT count(a.id)
                     FROM SpljBundle:Article a
                     WHERE a.userId = u.id
-                    AND a.status != 2
+                    AND a.status = 1
                     AND u.roles LIKE :teacher
                     GROUP BY u.username) AS articles,
                 (SELECT count(q.id)
                     FROM SpljBundle:question q, SpljBundle:mcq x
                     WHERE x.userId = u.id
-                    AND x.status != 2
+                    AND x.status = 1
                     AND u.roles LIKE :teacher
                     AND x.id = q.idQcm
                     GROUP BY u.username) AS questions 
@@ -115,7 +115,7 @@ class DefaultController extends Controller
 
         // chart articles
         $query = $em->createQuery(
-            'SELECT COUNT(a) FROM SpljBundle:Article a WHERE a.status != 2 GROUP BY a.userId'
+            'SELECT COUNT(a) FROM SpljBundle:Article a WHERE a.status = 1 GROUP BY a.userId'
             );
         $count = $query->getResult();
         $maxArticles = 0;
@@ -130,7 +130,7 @@ class DefaultController extends Controller
 
         // chart mcq
         $query = $em->createQuery(
-            'SELECT COUNT(m) FROM SpljBundle:Mcq m WHERE m.status != 2 GROUP BY m.userId'
+            'SELECT COUNT(m) FROM SpljBundle:Mcq m WHERE m.status = 1 GROUP BY m.userId'
             );
         $countMcq = $query->getResult();
         $maxMcq = 0;
@@ -144,7 +144,7 @@ class DefaultController extends Controller
 
         // chart questions
         $query = $em->createQuery(
-            'SELECT SUM(m.nbQuestions) FROM SpljBundle:Mcq m WHERE m.status != 2 GROUP BY m.userId'
+            'SELECT SUM(m.nbQuestions) FROM SpljBundle:Mcq m WHERE m.status = 1 GROUP BY m.userId'
             );
         $countQuestion = $query->getResult();
         $maxQuestion = 0;
